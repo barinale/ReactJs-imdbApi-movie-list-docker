440f6668ea2ffe83f06392cf3a950b095de71258
barinale <barinale08@gmail.com>
2023-11-05 14:27:52 +0100

    working in movie Detail

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
+import imageNotFound from './../assets/no_image_available.jpeg'
 
  export const MovieDetail = ({path_url}) => {
    let { id } = useParams()
+   
 const {data : movie}  =  useFetch(path_url+id)
-
+const image = movie.backdrop_path ? "https://image.tmdb.org/t/p/original/"+movie.poster_path : imageNotFound;
 console.log(movie)
 return (
     <main className="">
-      <section className='flex md:flex-row flex-col'>
+      <section className='flex justify-center flex-wrap py-5'>
         <div className='basis-1/4  bg-slate-500 '>
-          <img src="" width="200" height="200"/>
+          <img src={image} className='rounded' width="200" height="200"/>
         </div>
         <div className='basis-1/2 bg-slate-300 '>
           <h1>Name</h1>
