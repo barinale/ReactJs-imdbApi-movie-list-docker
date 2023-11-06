fba633d1a7b74eb853438e6f34898337222ae133
barinale <barinale08@gmail.com>
2023-11-06 13:09:39 +0100

    updating useEffect

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,20 @@
 import  { useEffect, useState } from 'react'
 
 export const useFetch = (path,query="") => {
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: process.env.REACT_APP_API_KEY
-        }
-      };
+  
       
     const [data,setData] = useState([])
     
     const url = `https://api.themoviedb.org/3/${path}?query=${query}&include_adult=false&language=en-US`
     
     useEffect(()=>{
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.REACT_APP_API_KEY
+        }
+      };
 
         async function fetchMovie(){
 
